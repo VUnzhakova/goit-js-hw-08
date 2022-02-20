a7877b2702a36e471b700bb49753096d1b31a826
Valentina Unzhakova <vunzhakova@outlook.com>
2022-02-20 18:42:31 +0200

    правки первое задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,24 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
 
 const galleryItemsEl = document.querySelector('.gallery');
 
-const selectorGallery = (images) => {
-    return images.map(({ preview, original, description }) => {
-        return `<li class="gallery__list"><a class="gallery__item" href=${original}><img class= "gallery__image" src="${preview}" alt="" title="${description}"></a></li>`
-    }).join('');
-}
+const selectorGallery = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+const createGalleryMarkup = galleryItems.map(selectorGallery).join('');
+galleryItemsEl.insertAdjacentHTML('beforeend', createGalleryMarkup);
 
-const cardImagesMarkup = selectorGallery(galleryItems);
-
-galleryItemsEl.insertAdjacentHTML("afterbegin", cardImagesMarkup);
-
-galleryItemsEl.addEventListener('click', selectGalleryEl);
-
-
-const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
-
-function selectGalleryEl(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-    lightbox;
-}
-
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,13 +5,12 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 const onPlay = function (event) {
-    localStorage.setItem("videoplayer-current-time", event.seconds);
-    };
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
 
-    player.on('timeupdate', throttle(onPlay, 1000));
-    
-    if (localStorage.getItem('videoplayer-current-time')) {
-        const currenTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-    player.setCurrentTime(currenTime);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-    }
+if (localStorage.getItem('videoplayer-current-time')) {
+  const currenTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(currenTime);
+}
